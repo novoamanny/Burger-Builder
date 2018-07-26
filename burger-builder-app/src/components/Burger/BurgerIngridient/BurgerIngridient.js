a19eadbf8b58949b83245d09f0996f2e18c89ad5
Manuel Novoa <manuelnovoa@Manuels-iMac.local>
2018-07-26 14:01:29 -0500

    Minor modifications - BurgerIngridient file was modified.

--- burger-builder-app/src/components/Burger/BurgerIngridient/BurgerIngridient.js
+++ burger-builder-app/src/components/Burger/BurgerIngridient/BurgerIngridient.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import classes from '.BurgerIngridient.css';
 
-const burgerIngridient = (props) => {
-    let ingridient = null;
+class BurgerIngridient extends Component{
 
-    // Check for Ingridient
-    switch(props.type){
-        case ('bread-bottom'):
-            ingridient = <div className={classes.BreadBottom}></div>;
-            break;
-        case ('bread-top'):
-            ingridient = (
-                <div className={classes.BreadTop}>
-                    <div className={classes.Seed1}></div>
-                    <div className={classes.Seed2}></div>
-                </div>
-            );
-            break;
-        case ('meat'):
-            ingridient = <div className={classes.Meat}></div>
-            break;
-        case ('cheese'):
-            ingridient = <div className={classes.Cheese}></div>
-            break;
-        case ('bacon'):
-            ingridient = <div className={classes.Bacon}></div>
-            break;
-        case ('salad'):
-            ingridient = <div className={classes.Salad}></div>
-            break;
-        default:
-            ingridient = null;
-    }
-};
+    render(){
+        let ingridient = null;
 
-export default burgerIngridient;
+        // Check for Ingridient
+        switch(this.props.type){
+            case ('bread-bottom'):
+                ingridient = <div className={classes.BreadBottom}></div>;
+                break;
+            case ('bread-top'):
+                ingridient = (
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seed1}></div>
+                        <div className={classes.Seed2}></div>
+                    </div>
+                );
+                break;
+            case ('meat'):
+                ingridient = <div className={classes.Meat}></div>
+                break;
+            case ('cheese'):
+                ingridient = <div className={classes.Cheese}></div>
+                break;
+            case ('bacon'):
+                ingridient = <div className={classes.Bacon}></div>
+                break;
+            case ('salad'):
+                ingridient = <div className={classes.Salad}></div>
+                break;
+            default:
+                ingridient = null;
+        }
+        return ingridient;
+    }     
+}
+
+// Prop type validation
+BurgerIngridient.propTypes = {
+    type: PropTypes.string.isRequired
+}
+
+export default BurgerIngridient;
