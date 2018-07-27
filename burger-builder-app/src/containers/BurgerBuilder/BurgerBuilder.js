8d1bc9d3f14ee1a8691e95994b5f78e5a0ead378
Manuel Novoa <manuelnovoa@Manuels-iMac.local>
2018-07-26 20:16:15 -0500

    All of the Build control buttons are able to add ingredients everytime the user presses the more button. The ingredients are displayed for the user everytime the button is pressed.

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,17 +3,38 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component{
     state = {
-        ingredient:{
+        ingredients:{
             salad: 0,
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
 
     addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const newCount = oldCount + 1;
+
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = newCount;
+        
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 
     }
 
@@ -24,8 +45,9 @@ class BurgerBuilder extends Component{
     render (){
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredient}/>
-                <BuildControls/>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}/>
             </Aux>
         );
     }
