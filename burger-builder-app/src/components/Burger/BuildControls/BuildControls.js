09edc3851f572881b1203a5307e5f40b482ffb79
Manuel Novoa <manuel.novoa@mavs.uta.edu>
2018-07-29 16:44:13 -0500

    The user is now allowed to remove ingredients from their order. In addition, the price is now displayed for the user to view. Price updates according to what the user add to their order.

--- burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder-app/src/components/Burger/BuildControls/BuildControls.js
@@ -23,13 +23,21 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+    {/* Price display */}
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        
+        {/* Controls */}
         {controls.map(ctrl => (
             <BuildControl 
                 key= {ctrl.label} 
                 label={ctrl.label}
-                 
-                added={() => props.ingredientAdded(ctrl.type)}/>
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}/>
         ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,21 @@ class BurgerBuilder extends Component{
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState(ingredients){
+        // array of values
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) =>{
+                return sum + el;
+            },0);
+        
+        this.setState({purchasable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -35,19 +49,48 @@ class BurgerBuilder extends Component{
         const newPrice = oldPrice + priceAddition;
 
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
 
     }
 
     removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0){
+            return;
+        }
+        const newCount = oldCount - 1;
 
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = newCount;
+        
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render (){
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        
+        // disable button
+        for (let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
+                    price={this.state.totalPrice}/>
             </Aux>
         );
     }
