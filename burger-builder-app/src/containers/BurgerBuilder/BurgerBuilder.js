e316103b383f4d75f431ed2a7a21b544c5b8df49
Manuel Novoa <manuelnovoa@Manuels-iMac.local>
2018-07-31 16:07:47 -0500

    Minor changes - Logo has been added on the top center of the application.

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -108,7 +108,8 @@ class BurgerBuilder extends Component{
                     <OrderSummary
                         ingredients={this.state.ingredients}
                         purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.PurchaseContinueHandler}/>
+                        purchaseContinued={this.PurchaseContinueHandler}
+                        price={this.state.totalPrice}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
